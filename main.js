1e491737b826eb60d2bcd57c05f07cecac7edbd0
zanebliss <zanebliss@icloud.com>
2020-04-24 16:01:06 -0500

    init commit

---
+++ main.js
@@ -0,0 +1,48 @@
+
+
+const DragDropManager = Object.create(null, {
+  init: {
+    value: () => {
+      const stages = document.querySelectorAll(".stage");
+
+      stages.forEach((stage) => {
+        // Gain reference of item being dragged
+        stage.ondragstart = (e) => {
+          e.dataTransfer.setData("text", e.target.classList);
+        };
+      });
+
+      const targets = document.querySelectorAll(".target");
+
+      targets.forEach((target) => {
+        // Dragover not supported by default. Turn that off.
+        target.ondragover = (e) => e.preventDefault();
+
+        target.ondrop = (e) => {
+          // Enabled dropping on targets
+          e.preventDefault();
+
+          // Determine what's being dropped
+          const data = e.dataTransfer.getData("text");
+
+          // Append card to target component as child
+          // TODO: This should only happen if the target has no children nodes
+          
+          if (e.target.className.includes("origin")) {
+            e.target.appendChild(
+                document.querySelector(`.${data.split(" ")[1]}`)
+            )
+          } 
+         else if (e.target.hasChildNodes()) {
+              window.alert("Already full. Don't drop on a box, only empty space!")
+          } else {
+              e.target.appendChild(
+                document.querySelector(`.${data.split(" ")[1]}`)
+              );
+          }
+        };
+      });
+    },
+  },
+});
+DragDropManager.init();
